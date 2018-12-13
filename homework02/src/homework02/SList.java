01986d8d91aa210fcbe1107071ee4518d8aeae62
DmytroKyslyak <dmytro.kyslyak@gmail.com>
2018-12-13 14:25:20 +0200

    hw02_2
    hw02_2

--- homework02/src/homework02/SList.java
+++ homework02/src/homework02/SList.java
@@ -6,7 +6,7 @@ package homework02;
 		
 		
 		public SList() {
-			student = new SStudent[5];
+			student = new SStudent[15];
 			count=-1;
 		}
 		public void add(SStudent st) {
@@ -44,4 +44,57 @@ package homework02;
 			 }		 
 			 return studentsByFacultet;
 		 }
+		
+		 public void printSort() {
+			 for (int i = 0; i < size(); i++) {
+				  System.out.println(student[i]); 
+			 }		 
+		 }
+		/*
+		 public SList Sort() {
+			 SList sort = new SList();
+			 for (int i = 0; i < size(); i++) {
+				if (student[i].getKurs()==0) {
+				 
+					 sort.add(student[i]); 
+					 }
+			 }		 
+			 return sort;
+		 } 
+		 */
+		 
+		 
+		 
+		 public void printStudentsByYear(int year) {
+			 for (int i = 0; i < size(); i++) {
+				 if (student[i].getYear()>=year) {
+					 System.out.println(student[i]); }
+			 }		 
+		 }
+		 public SList getStudentsByYear(int year) {
+			 SList studentsByYear = new SList();
+			 for (int i = 0; i < size(); i++) {
+				 if (student[i].getYear()>=year) {
+					 studentsByYear.add(student[i]); }
+			 }		 
+			 return studentsByYear;
+		 } 
+		 
+		 
+		 public void printStudentsByGroup(String group) {
+			 for (int i = 0; i < size(); i++) {
+				 if (student[i].getGroup().equals(group)) {
+					 System.out.println(student[i]); }
+			 }		 
+		 }
+		 public SList getStudentsByGroup(String group) {
+			 SList studentsByGroup = new SList();
+			 for (int i = 0; i < size(); i++) {
+				 if (student[i].getGroup().equals(group)) {
+					 studentsByGroup.add(student[i]); }
+			 }		 
+			 return studentsByGroup;
+		 } 
+		 
+		 
 }
--- homework02/src/homework02/SMain.java
+++ homework02/src/homework02/SMain.java
@@ -9,13 +9,35 @@ public class SMain {
 		System.out.println(st1);
 		*/
 		SList st = new SList();
-		st.add(new SStudent("FIFT", 5, "ST-51", 556677, "Ivanov", "Jack", 2001, "Kiev", 8333433));
-		st.add(new SStudent("FIOT", 4, "ST-41", 558877, "Petrov", "Ivan", 2002, "Lviv", 4220433));
-		st.add(new SStudent("FIFT", 3, "ST-51", 5568677, "Ivanov", "Jack", 2004, "Kiev", 433));
+		st.add(new SStudent("IPSA", 5, "ST-51", 556677, "Ivanov", "Jack", 2001, "Kiev", 3833343));
+		st.add(new SStudent("IPSA", 5, "SM-51", 556677, "Daniels", "Jack", 2001, "Kiev", 8333433));
+		st.add(new SStudent("IPSA", 4, "ST-41", 558877, "Smirnoff", "Ivan", 2002, "Lviv", 4220433));
+		st.add(new SStudent("RTF", 3, "SD-51", 5568677, "Ivanov", "Gleb", 2005, "Kiev", 9998433));
+		st.add(new SStudent("RTF", 4, "ST-41", 558877, "Petrov", "Marat", 2002, "Lviv", 4220433));
+		st.add(new SStudent("FIVT", 4, "ST-41", 558877, "Lincoln", "Ivan", 2002, "Lviv", 4220433));
+		st.add(new SStudent("FIVT", 3, "ST-51", 5568677, "Franklin", "Jack", 2004, "Kiev", 7777433));
+		st.add(new SStudent("FIVT", 5, "SD-51", 556677, "Ivanov", "Stss", 2001, "Kiev", 8333433));
+		st.add(new SStudent("RTF", 4, "ST-41", 558877, "Petrov", "Ivan", 2002, "Lviv", 4220433));
+		st.add(new SStudent("RTF", 3, "SD-51", 5568677, "Ivanov", "Greg", 2005, "Kiev", 9998433));
+		st.add(new SStudent("RTF", 3, "ST-51", 5568677, "Ivanov", "Alex", 2000, "Kiev", 9998433));
+		st.add(new SStudent("FIVT", 3, "SK-51", 5568677, "Jamson", "Jack", 2000, "Kiev", 9998433));
+		st.add(new SStudent("FIVT", 5, "SD-51", 556677, "Washington", "Jack", 2001, "Kiev", 3833343));
+		st.add(new SStudent("IPSA", 4, "SM-41", 558877, "Petrov", "Ivan", 2002, "Lviv", 4220433));
+		st.add(new SStudent("IPSA", 3, "ST-51", 5568677, "Sidorov", "Jack", 2004, "Kiev", 7777433));
 		
-		st.print();
+		
+		//st.print();
+		//System.out.println();
+	
+		st.printStudentsByFacultet("RTF");
 		System.out.println();
-		st.printStudentsByFacultet("FIFT");
+		st.printSort();
+		System.out.println();
+		st.printStudentsByYear(2005);
+		System.out.println();
+		st.printStudentsByGroup("ST-51");
+	
+
 	}
 
 }
