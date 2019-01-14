fefe7960cdba97a2de46546adcae00a5ac544e20
DmytroKyslyak <dmytro.kyslyak@gmail.com>
2019-01-14 12:13:53 +0200

    hw_05_1
    hw_05_1

--- prj05JDBC/src/ua/univer/database/Map.java
+++ prj05JDBC/src/ua/univer/database/Map.java
@@ -2,18 +2,35 @@ package ua.univer.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Map {
 	Connection con;
 	Statement stmt;
-	
-	public Map() {
-		Class.forName("com.mysql.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/3306/mapdb", "root", "1");
-		stmt = con.createStatment();
-		
+public Map() throws Exception {
+	Class.forName("com.mysql.jdbc.Driver");
+	con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mapdb", "root", "root");
+	stmt= con.createStatement();
+}
+public void show() throws SQLException{
+	String sql = "SELECT * FROM coutries";
+	ResultSet res = stmt.executeQuery(sql);
+	while(res.next()){
+		int id =res.getInt("ID_CO");
+		String name = res.getString("name");
+		Country country = new Country(id,name);
+		System.out.println(country);
 	}
+}
+public void addCountry(Country country) throws SQLException{
+	String sql = "insert into coutries values("+country.getId()+", '"+country.getName()+"')";
+	stmt.executeUpdate(sql);
+}
 
+public void deleteCountry(String name) throws SQLException{
+	String sql = "DELETE FROM coutries WHERE name='"+name+"'";
+	stmt.executeUpdate(sql);
+}
 }
--- prj05JDBC/src/ua/univer/database/Program.java
+++ prj05JDBC/src/ua/univer/database/Program.java
@@ -1,8 +1,16 @@
 package ua.univer.database;
 
 public class Program {
-	public static void(String[] arg) {
-		
+public static void main(String[] args) {
+	try {
+		Map map = new Map();
+		map.show();
+		map.addCountry(new Country(4, "UK"));
+		map.deleteCountry("RUS");
+		map.show();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-
+}
 }
