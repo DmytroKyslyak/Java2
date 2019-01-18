6e46b994ef0e94c3ce23dbbe7b43654392613436
DmytroKyslyak <dmytro.kyslyak@gmail.com>
2019-01-18 14:46:38 +0200

    hw_05_2
    hw_05_2

--- prj05/src/ua/univer/weather/station/Program.java
+++ prj05/src/ua/univer/weather/station/Program.java
@@ -6,14 +6,20 @@ import ua.univer.weather.station.model.display.CurrentDisplay;
 import ua.univer.weather.station.view.WeatherViewConsole;
 
 public class Program {
-
+	
 	public static void main(String[] args) throws InterruptedException {
+		
+		
 		WeatherDataControl weatherKiev = new WeatherDataControl();
 		WeatherDataControl weatherLviv = new WeatherDataControl();
 		CurrentDisplay currentDisplayKiev = new CurrentDisplay(weatherKiev);
 		CurrentDisplay currentDisplayLviv = new CurrentDisplay(weatherLviv, new WeatherViewConsole());
 		CurrentDisplay currentDisplayAll = new CurrentDisplay(weatherKiev);
 		currentDisplayAll.setSubject(weatherLviv);
+		
+		weatherKiev.setWeather(new WeatherEntity("Kiev",30, 50, 750));
+		
+		/*
 		for (int i = 0; i < 20; i++) {
 			weatherKiev.setWeather(new WeatherEntity("Kiev",i, 50+i, 750+i));
 			Thread.sleep(400);
@@ -21,8 +27,21 @@ public class Program {
 			Thread.sleep(400);
 			if(i==10) currentDisplayAll.setViewer(new WeatherViewConsole());
 		}
-				
+			*/	
 
 	}
-
+/*	
+public static void main(String[] args) {
+	try {
+		Univer univer = new WeatherEntity();
+		univer.show();
+		//univer.addGroup(new group(5, "Python"));
+		//univer.deleteGroup("Python");
+		univer.show();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+*/
 }
